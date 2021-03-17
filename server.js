5a3a182dc8fb0df5ee6742e3cc53a1891e42740c
Johannes Weis <joh.weis.18@lehre.mosbach.dhbw.de>
2021-03-17 18:33:39 +0100

    add mongodb

--- server.js
+++ server.js
@@ -26,3 +26,17 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT)
 })
+
+const db = require('./models')
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to the database')
+  })
+  .catch(err => {
+    console.log('Cannot connect to the database', err)
+    process.exit()
+  })
